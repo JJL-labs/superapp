0a71cda151961aad5dc843dc62aa9c2b85672c0b
RhayssaGondim <rhayssagondim@hotmail.com>
2025-02-10 18:47:51 -0300

    init project and add splash screen

---
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jetbrains.kmpapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContent {
+            // Remove when https://issuetracker.google.com/issues/364713509 is fixed
+            LaunchedEffect(isSystemInDarkTheme()) {
+                enableEdgeToEdge()
+            }
+            App()
+        }
+    }
+}
